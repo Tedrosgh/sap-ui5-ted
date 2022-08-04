ad5914e7fd7e7b73da85226a7be5637425610775
Tedrosgh <tedrosgh@yahoo.com>
2022-08-04 02:01:55 -0600

    16-17 Fragment - controlled

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+], function(Controller, MessageToast, Fragment) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel",{
     omShowHello : function (){
@@ -10,6 +11,30 @@ sap.ui.define([
         var sMsg = oBundle.getText("helloMsg", [sRecipient])
         //show message
        MessageToast.show(sMsg)
+    },
+    onOpenDialog : function () {
+        var oView = this.getView();
+
+        //create the dialog lazily
+
+        if (!this.byId("helloDialog")) {
+            //load asynchronous XML fragment
+            Fragment.load({
+                id: oView.getId(),
+                name: "sap.ui.demo.walkthrough.view.HelloDialog",
+                //connect to controller
+                controller: this
+            }).then(function (oDialog) {
+                //Connect dialog to the root view of this componenet (models, lifecycle)
+                oView.addDependent(oDialog);
+                oDialog.open();
+            })
+        }else {
+            this.byId("helloDialog").open();
+        }
+    },
+    onCloseDialog : function () {
+        this.byId("helloDialog").close();
     }
 })
 });
